bbe42359ed1f42428a12dceae5f1a5d3253580ce
yangzihan613 <yangzihan790@gmail.com>
2024-11-14 21:51:22 +0800

    Hw4

--- P37/source/main.c
+++ P37/source/main.c
@@ -1,30 +1,42 @@
 #include <stdio.h>
-#include <stdbool.h>
+#include <stdlib.h>
 
-void bubbleSortImproved2(int arr[], int n) {
-    int i, j, temp;
-    bool swapped;
-    for (i = 0; i < n - 1; i++) {
-        swapped = false;
-        for (j = 0; j < n - i - 1; j++) {
-            if (arr[j] > arr[j + 1]) {
-                temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
-                swapped = true;
-            }
-        }
-        if (!swapped)
-            break; // 如果在一次遍歷中沒有發生交換，則退出循環
+#define SIZE 100
+
+int linearSearch(const int array[], int key, int size);
+
+int main(void) {
+    int a[SIZE];
+    int x;
+    int searchKey;
+    int element;
+
+    for (x = 0; x < SIZE; x++) {
+        a[x] = 2 * x;
     }
+
+    printf("Enter integer search key:\n");
+    scanf("%d", &searchKey);
+
+    element = linearSearch(a, searchKey, SIZE);
+
+    if (element != -1) {
+        printf("Found value in element %d\n", element);
+    }
+    else {
+        printf("Value not found\n");
+    }
+
+    system("pause");
+    return 0;
 }
 
-int main() {
-    int arr[] = { 64, 34, 25, 12, 22, 11, 90 };
-    int n = sizeof(arr) / sizeof(arr[0]);
-    bubbleSortImproved2(arr, n);
-    printf("排序後的數組: \n");
-    for (int i = 0; i < n; i++)
-        printf("%d ", arr[i]);
-    return 0;
+int linearSearch(const int array[], int key, int size) {
+    int n;
+    for (n = 0; n < size; n++) {
+        if (array[n] == key) {
+            return n;
+        }
+    }
+    return -1;
 }
